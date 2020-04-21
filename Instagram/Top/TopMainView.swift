bda482b12916c84118f172318ed57cd90390dd06
terasawa <a.terasawa@a-sw.jp>
2020-04-21 14:46:04 +0900

    create topPage

--- Instagram/Top/TopMainView.swift
+++ Instagram/Top/TopMainView.swift
@@ -23,12 +23,29 @@ class TopMainView: BaseView {
 extension TopMainView {
     override func awakeFromNib() {
         super.awakeFromNib()
+        setDelegate()
+        loadTableViewCellFromXib(tableView: tableView, cellName: "TopMainTableViewCell")
     }
 }
 // MARK: - Protocol
-extension TopMainView {
+extension TopMainView:UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "TopMainTableViewCell", for: indexPath) as?
+            TopMainTableViewCell else {return UITableViewCell() }
+        return cell
+    }
+    
 }
+
+
 
 // MARK: - method
 extension TopMainView {
+    func setDelegate() {
+        tableView.dataSource = self
+    }
 }
